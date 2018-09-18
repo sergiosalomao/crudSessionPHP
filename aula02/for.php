9758d3b610c44fff1a0f7dbf793a12f41a4359b6
Sergio Salomao <salomao.leite@hotmail.com>
2018-09-18 19:18:15 -0300

    ajuste

--- aula02/for.php
+++
@@ -1,5 +0,0 @@
-<?php
-//$quantidade = count(array('a'=>'aaa','b'=>'aaa'));
-for ($x=0;$quantidade;$x++){
-    echo "$x <br>" ;
-}
--- aula04_/inserir.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION['alunos']= $_SESSION['alunos'] ?? [];
-
-array_push($_SESSION['aluno'],$_POST);
-
-header('location:index.php');
